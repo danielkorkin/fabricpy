b4d248e3d211bac843fad49a5ebf0b809bbcad90
devium335 <108692136+devium335@users.noreply.github.com>
2025-06-11 12:04:45 -0400

    v0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "fabricpy"
 copyright = "2025, Daniel Korkin"
 author = "Daniel Korkin"
-release = "v0.1.1"
+release = "v0.1.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
